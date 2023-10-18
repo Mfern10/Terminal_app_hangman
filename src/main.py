2c65d0a9a57bc61724fa98c284f9480b0038091a
Mfern10 <mitchellfernandez147@gmail.com>
2023-10-18 18:22:27 +0900

    Created a list of words, select a random word and display blank word

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import random
+# Create Word List for the games words
+word_list = [
+    "apple",
+    "banana",
+    "strawberry",
+    "pineapple",
+    "mango",
+    "peach"
+]
+
+# Select a random word from the List
+random_word = random.choice(word_list)
+
+print(random_word)
+
+# Display empty word space with underscores
+print("_ " * (len(random_word)))
+
+#Variables needed for the game
+max_tries = 6
+tries = 0
+guessed_letters = []
+
+
+
+
+
+
+
+
