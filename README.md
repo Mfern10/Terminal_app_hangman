1ccb67bf723cf3c3058858e1021fa815ac8d8880
Mfern10 <mitchellfernandez147@gmail.com>
2023-10-19 13:19:03 +0900

    Added correct guess function to update the while loop if guess is correct updates displayed word with letters

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HANGMAN GAME TERMINAL APPLICATION
+
+
+
+
+## References
+https://stackoverflow.com/questions/4394145/picking-a-random-word-from-a-list-in-python
--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ print(selected_word)
 
 # Display the selected word as undercores
 def initialize_displayed_word(word):
-    return "_ " * len(word)
+    return "_" * len(word)
 
 displayed_word = initialize_displayed_word(selected_word)
 print(displayed_word)
@@ -34,11 +34,43 @@ def failed_game():
     print("GAME OVER!")
     exit(1)
 
+# This function is checking that the guess is within the selected word, 
+# if correct saving it to local iteration variable then returning the iteration variable once all letters are found
+def correct_guess(correct_letter):
+    iteration = displayed_word
+    for i in range(len(selected_word)):
+        if correct_letter == selected_word[i]:
+            lst = list(iteration)
+            lst[i] = correct_letter
+            iteration = ("".join(lst))
+    return  iteration
+   
+            
+
+    
+def incorrect_guess():
+    pass
+#     if guess not in selected_word:
+#         guessed_letters.append(guessed)
+#         print(guessed_letters)
+#     else:
+#         for guess in guessed_letters:
+#              current_tries += 1
+
+    
+
 while True:
     guess = input("Guess a Letter: ")
-    current_tries += 1
-    if current_tries == max_incorrect_guesses:
-        failed_game()
+    if guess in guessed_letters:
+        print("You guessed this letter already, try again!")
+    else:
+        if guess in selected_word:
+            displayed_word = correct_guess(guess) # update display word to show correct letter/ replace _ 
+            print(displayed_word)
+        else:
+            incorrect_guess() # calls incorrect guess function updates counter
+
+ 
 
     
 
