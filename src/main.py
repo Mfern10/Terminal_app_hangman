15453e2208571f36742e857f1454236c26a23e2b
Mfern10 <mitchellfernandez147@gmail.com>
2023-10-20 13:55:27 +0900

    Added an Initialize game function and loop to ask user if they want to start the game or exit

--- src/main.py
+++ src/main.py
@@ -11,11 +11,6 @@ word_list = [
     "peach"
 ]
 
-ascii_banner = pyfiglet.figlet_format("HANGMAN")
-print(ascii_banner)
-
-print(""" <------Can you guess the secret word!-----> """)
-
 # Select a random word from the List
 def select_word(word_list):
     return random.choice(word_list)
@@ -36,11 +31,12 @@ max_incorrect_guesses = 5
 incorrect_guesses = 0
 guessed_letters = []
 
-# Start game loop using while loop
+# call this function when the player fails
 def failed_game():
     print("GAME OVER!")
     sys.exit(1)
 
+# Call this function when the player wins
 def won_game():
     print("Congratulations! You have won the game!")
     sys.exit(0)
@@ -61,25 +57,55 @@ def correct_guess(correct_letter):
 # and display guessed letters guessed on screen
 def incorrect_guess(wrong_letter):
     guessed_letters.append(wrong_letter)
-    print(guessed_letters)
+    print(f"These are your previous guesses! {guessed_letters}")
+
+
+ascii_banner = pyfiglet.figlet_format("HANGMAN!")
+print(ascii_banner)
+
+
+
+def main_menu():
+    print(""" <------Can you guess the secret word!-----> """)
+    print("1. Play")
+    print("2. Exit")
+
+    choice = input("Enter your choice: ")
+    return choice
+
+
     
 
-# while loop runs main function of game asks user to guess and checks correct or incorrect and calls correct functions 
+# Main Menu Initalises game and asks user if they want to play!  
 while True:
-    guess = input("Guess a Letter: ")
-    if guess in guessed_letters:
-        print("You guessed this letter already, try again!")
-    else:
-        if guess in selected_word:
-            incorrect_guesses -= 1
-            displayed_word = correct_guess(guess) # update display word to show correct letter/ replace _ 
-            print(displayed_word)
-        if "_" not in displayed_word:
-                won_game()
+    user_choice = main_menu()
+    if user_choice == "1":
+       selected_word = select_word(word_list)
+       displayed_word = initialize_displayed_word(selected_word)
+
+       print("New Game! Goodluck!", displayed_word)
+    elif user_choice == "2":
+        print("Thanks for playing!")
+        break 
+    else: 
+        print("Invalid choice. Please select a valid choice.")
+        
+    # Main game loop runs only if user asked to play, uses conditions and above functions for how to win or lose
+    while True:        
+        guess = input("Guess a Letter: ")
+        if guess in guessed_letters:
+            print("You guessed this letter already, try again!", end = " ")
         else:
-            if incorrect_guesses == max_incorrect_guesses:
-                failed_game()
+            if guess in selected_word:
+                incorrect_guesses -= 1
+                displayed_word = correct_guess(guess) # update display word to show correct letter/ replace _ 
+                print(displayed_word)
+            if "_" not in displayed_word:
+                won_game()
             else:
-                incorrect_guesses += 1
-                incorrect_guess(guess) # calls incorrect guess function updates counter
-                
+                if incorrect_guesses == max_incorrect_guesses:
+                    failed_game()
+                else:
+                    incorrect_guesses += 1
+                    incorrect_guess(guess) # calls incorrect guess function updates counter
+           
